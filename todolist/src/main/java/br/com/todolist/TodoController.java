fcdb35d447265f1c50a6fbaea9ef254fc6be52bc
Caio Zein <caio.zein@icloud.com>
2023-08-11 11:21:12 -0300

    Criando endpoint para listar as tarefas

---
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -0,0 +1,27 @@
+package br.com.todolist;
+
+import org.aspectj.apache.bcel.classfile.Code;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@CrossOrigin("*")
+public class TodoController {
+
+    private final TodoRepository todoRepo;
+
+    public TodoController(TodoRepository todoRepo) {
+        this.todoRepo = todoRepo;
+    }
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Todo> GetAll() {
+
+        return this.todoRepo.findAll();
+
+    }
+}
